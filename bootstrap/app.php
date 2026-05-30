e0bd1bf7dbdb5128e38b6973d8974aa50f4a2245
Richard <rialmon@gmail.com>
2026-05-29 19:36:36 -0500

    Instalación JWT con 'composer require php-open-source-saver/jwt-auth' siguiendo 'https://certidevs.com/tutorial-laravel-php-autenticacion-jwt-api-rest'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use PHPOpenSourceSaver\JWTAuth\Http\Middleware\Authenticate as JWTAuthenticate;
+use PHPOpenSourceSaver\JWTAuth\Http\Middleware\RefreshToken as JWTRefreshToken;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'jwt.auth'    => JWTAuthenticate::class,
+            'jwt.refresh' => JWTRefreshToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
